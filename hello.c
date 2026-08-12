4e5e9a8a1e60c96662f4ed3c45518862b0315cee
Aaron <vargasaaron29000@gmail.com>
2026-08-12 16:39:04 +0800

    Added my first C program.

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("====================\n C programming!\n====================\n \t Name: Aaron\n \t Language: C\n \t Status: Learning\n ");
+    return 0;
+}
